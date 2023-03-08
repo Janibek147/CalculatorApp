d83630807a5d4c331028ce4da9b5b807875df799
Nurlibay <koshkinbayevn@gmail.com>
2023-03-09 00:57:01 +0500

    multiple number calculation functionality has done!

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -21,35 +21,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnEight: Button
     private lateinit var btnNine: Button
 
+    private lateinit var btnPlus: Button
+    private lateinit var btnMinus: Button
+    private lateinit var btnMul: Button
+    private lateinit var btnDivide: Button
+    private lateinit var btnPercent: Button
+    private lateinit var btnClear: Button
+    private lateinit var btnBackspace: Button
+    private lateinit var btnEquals: Button
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        input = findViewById(R.id.input_console)
-        output = findViewById(R.id.result)
-
-        btnZero = findViewById(R.id.btn_zero)
-        btnOne = findViewById(R.id.btn_one)
-        btnTwo = findViewById(R.id.btn_two)
-        btnThree = findViewById(R.id.btn_three)
-        btnFour = findViewById(R.id.btn_four)
-        btnFive = findViewById(R.id.btn_five)
-        btnSix = findViewById(R.id.btn_six)
-        btnSeven = findViewById(R.id.btn_seven)
-        btnEight = findViewById(R.id.btn_eight)
-        btnNine = findViewById(R.id.btn_nine)
-
-        val btnPlus = findViewById<Button>(R.id.btn_plus)
-        val btnMinus = findViewById<Button>(R.id.btn_minus)
-        val btnMul = findViewById<Button>(R.id.btn_mul)
-        val btnDevide = findViewById<Button>(R.id.btn_devide)
-        val btnPercent = findViewById<Button>(R.id.btn_percent)
-        val btnClear = findViewById<Button>(R.id.btn_ac)
-        val btnBackspace = findViewById<Button>(R.id.btn_backsapce)
-        val btnEquals = findViewById<Button>(R.id.btn_equals)
-
-
+        initView()
 
         btnZero.setOnClickListener {
             addToInputText("0")
@@ -101,10 +87,108 @@ class MainActivity : AppCompatActivity() {
             input.text = removeLast
         }
 
+        btnPlus.setOnClickListener {
+            addToInputText("+")
+        }
+
+        btnMinus.setOnClickListener {
+            addToInputText("-")
+        }
+
+        btnDivide.setOnClickListener {
+            addToInputText("/")
+        }
+
+        btnMul.setOnClickListener {
+            addToInputText("*")
+        }
+
+        btnEquals.setOnClickListener {
+            // 12 + 15 * 5
+            var operation = ""
+            // +++--
+            input.text.forEach {
+                if (!it.isDigit()) operation += it
+            }
+            val text = input.text.toString()
+            val numbers = text.split("+", "-", "*", "/").toMutableList()
+
+            var result = 0L
+
+            for(i in operation.indices){
+                if(i == 0) {
+                    result = calculate(numbers[i].toInt(), numbers[1].toInt(), operation[i])
+                } else {
+                    result = calculateResult(result.toInt(), numbers[i + 1].toInt(), operation[i])
+                }
+            }
+
+            output.text = result.toString()
+        }
+    }
+
+    private fun calculate(number1: Int, number2: Int, operation: Char): Long {
+        when(operation){
+            '+' -> {
+                return (number1.toDouble() + number2).toLong()
+            }
+            '-' -> {
+                return (number1.toDouble() - number2).toLong()
+            }
+            '*' -> {
+                return (number1.toDouble() * number2).toLong()
+            }
+            '/' -> {
+                return (number1.toDouble() / number2).toLong()
+            }
+        }
+        return 0L
+    }
+
+    private fun calculateResult(result: Int, nextNumber: Int, operation: Char): Long {
+        when(operation){
+            '+' -> {
+                return (result.toDouble() + nextNumber).toLong()
+            }
+            '-' -> {
+                return (result.toDouble() - nextNumber).toLong()
+            }
+            '*' -> {
+                return (result.toDouble() * nextNumber).toLong()
+            }
+            '/' -> {
+                return (result.toDouble() / nextNumber).toLong()
+            }
+        }
+        return 0L
+    }
+
+    private fun initView() {
+        input = findViewById(R.id.input_console)
+        output = findViewById(R.id.result)
+        btnZero = findViewById(R.id.btn_zero)
+        btnOne = findViewById(R.id.btn_one)
+        btnTwo = findViewById(R.id.btn_two)
+        btnThree = findViewById(R.id.btn_three)
+        btnFour = findViewById(R.id.btn_four)
+        btnFive = findViewById(R.id.btn_five)
+        btnSix = findViewById(R.id.btn_six)
+        btnSeven = findViewById(R.id.btn_seven)
+        btnEight = findViewById(R.id.btn_eight)
+        btnNine = findViewById(R.id.btn_nine)
+
+        btnPlus = findViewById(R.id.btn_plus)
+        btnMinus = findViewById(R.id.btn_minus)
+        btnMul = findViewById(R.id.btn_mul)
+        btnDivide = findViewById(R.id.btn_devide)
+        btnPercent = findViewById(R.id.btn_percent)
+        btnClear = findViewById(R.id.btn_ac)
+        btnBackspace = findViewById(R.id.btn_backsapce)
+        btnEquals = findViewById(R.id.btn_equals)
     }
 
     @SuppressLint("SetTextI18n")
     private fun addToInputText(value: String) {
-        input.setText(value + input.text.toString())
+        input.text = input.text.toString() + value
     }
 }
